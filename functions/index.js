90b18ab17b4cca06379212fb7087edcd44b2e356
Rafek91 <radek.chlud@gmail.com>
2023-12-12 11:28:45 +0100

    setup firebase express server + first test endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,11 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const express = require('express')
+const cors = require('cors')
+const functions = require('firebase-functions')
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const app = express()
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+app.get('/', (req, res) => {
+    res.send('Ahoj')
+})
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.app = functions.https.onRequest(app)
